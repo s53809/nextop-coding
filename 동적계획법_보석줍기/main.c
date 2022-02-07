5540e4cea115bd29db481d29d7feb94b58467ef8
Minsoda <dg4res@gmail.com>
2022-02-07 12:31:51 +0900

    Upload

--- 동적계획법_보석줍기/main.c
+++ 동적계획법_보석줍기/main.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
+#define N 20
+
+int A[N + 1][N + 1], R[N + 1][N + 1];
+int n, k;
+
+void input() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			scanf("%d", &A[i][j]);
+		}
+	}
+}
+
+void run() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			R[i][j] = R[i][j - 1] + R[i - 1][j] - R[i - 1][j - 1] + A[i][j];
+		}
+	}
+}
+
+void print() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			printf("%3d", R[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	printf("Hello, World!");
+	
+	scanf("%d%d", &n, &k);
+	input();
+	run();
+	print();
 }
