9b6f7d1eca0b674f3d3cab4f5759b8cf6fe69827
Minsoda <dg4res@gmail.com>
2022-02-07 12:02:32 +0900

    Upload

--- nextop-coding/cmain.c
+++ nextop-coding/cmain.c
@@ -5,26 +5,38 @@ struct abc {
 	int a, b;
 }A[N];
 
-int R[N];
+int R[N], n, B[N];
+
+void print() {
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", B[i]);
+	}
+}
 
 int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
-	int n;
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
 		R[i] = 0;
+		B[i] = 0;
 		scanf("%d%d", &A[i].a, &A[i].b);
 	}
-	int max = 0;
+	int max = 0, imsi = 0;
 	for (int i = 1; i <= n; i++) {
 		max = 0;
+		imsi = 0;
 		for (int j = i - 1; j >= 1; j--) {
 			if (A[i].b > A[j].b && R[j] > max) {
 				max = R[j];
+				imsi = j;
 			}
 		}
 		R[i] = max + 1;
+		B[i] = imsi;
 	}
+
+	print();
+
 	return 0;
 }
---
+++ 동적계획법_보석줍기/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	printf("Hello, World!");
+}
